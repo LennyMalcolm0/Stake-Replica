9243f99b79c53de064b39c036a209ef8bebddf44
Lenny-Prosper <leonardmalcolm00@gmail.com>
2022-12-23 17:56:30 +0000

    Odds Page Fixed Head

--- stake.js
+++ stake.js
@@ -1,52 +1,65 @@
 // VARIABLES AND FUNCTIONS FOR FIXED LEFT SIDE BAR
+function leftSideMenu() {
+    const casino = document.querySelector(".casino-main"),
+    casinoContainer = document.querySelector(".fl-casino"),
+    sports = document.querySelector(".sports-main"),
+    sportsContainer = document.querySelector(".fl-sports"),
+    promotions = document.querySelector(".promotions-main"),
+    promotionsContainer = document.querySelector(".fl-promotions"),
+    sponsorships = document.querySelector(".sponsorships-main"),
+    sponsorshipsContainer = document.querySelector(".fl-sponsorships"),
+    activeDropdown = document.querySelectorAll(".fl-dd-element");
 
-const casino = document.querySelector(".casino-main"),
-casinoContainer = document.querySelector(".fl-casino"),
-sports = document.querySelector(".sports-main"),
-sportsContainer = document.querySelector(".fl-sports"),
-promotions = document.querySelector(".promotions-main"),
-promotionsContainer = document.querySelector(".fl-promotions"),
-sponsorships = document.querySelector(".sponsorships-main"),
-sponsorshipsContainer = document.querySelector(".fl-sponsorships"),
-activeDropdown = document.querySelectorAll(".fl-dd-element");
+    casino.addEventListener("click", function(){
+        casinoContainer.classList.toggle("fl-active");
+    });
+    
+    sports.addEventListener("click", function(){
+        sportsContainer.classList.toggle("fl-active");
+    });
+    
+    promotions.addEventListener("click", function(){
+        promotionsContainer.classList.toggle("fl-active");
+    });
+    
+    sponsorships.addEventListener("click", function(){
+        sponsorshipsContainer.classList.toggle("fl-active");
+    });
 
-casino.addEventListener("click", function(){
-    casinoContainer.classList.toggle("fl-active");
-});
+    activeDropdown.forEach(btn => {
+        btn.addEventListener("click", function(){
+            document.querySelector(".fl-dropdown .fl-dropdown-active").classList.remove("fl-dropdown-active");
+            btn.classList.add("fl-dropdown-active");
+        })
+    });
 
-sports.addEventListener("click", function(){
-    sportsContainer.classList.toggle("fl-active");
-});
 
-promotions.addEventListener("click", function(){
-    promotionsContainer.classList.toggle("fl-active");
-});
+    const selectLang = document.querySelector(".fl-select-lang"),
+    langToggle = document.querySelector(".select"),
+    langChange = document.querySelector(".select .select-text"),
+    langOptions = document.querySelectorAll(".lang-options");
 
-sponsorships.addEventListener("click", function(){
-    sponsorshipsContainer.classList.toggle("fl-active");
-});
-
-activeDropdown.forEach(btn => {
-    btn.addEventListener("click", function(){
-        document.querySelector(".fl-dropdown .fl-dropdown-active").classList.remove("fl-dropdown-active");
-        btn.classList.add("fl-dropdown-active");
-    })
-});
-
-const selectLang = document.querySelector(".select-dropdown"),
-langToggle = document.querySelector(".select"),
-langChange = document.querySelector(".select .select-text"),
-langOptions = document.querySelectorAll(".lang-options");
-
-langToggle.addEventListener("click", function(){
-    selectLang.classList.toggle("lang-dd-active");
-});
-
-langOptions.forEach(lang => {
-    lang.addEventListener("click", function(){
-        langChange.innerHTML = lang.innerHTML;
-        document.querySelector(".select-dropdown .active-lang").classList.remove("active-lang");
-        lang.classList.add("active-lang");
+    langToggle.addEventListener("click", function(){
         selectLang.classList.toggle("lang-dd-active");
     });
-});
+    
+    langOptions.forEach(lang => {
+        lang.addEventListener("click", function(){
+            langChange.innerHTML = lang.innerHTML;
+            document.querySelector(".select-dropdown .active-lang").classList.remove("active-lang");
+            lang.classList.add("active-lang");
+            selectLang.classList.toggle("lang-dd-active");
+        });
+    });
+};
+
+leftSideMenu();
+
+
+
+
+
+
+
+
+
