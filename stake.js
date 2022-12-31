1e9c15a1b02e83a3408cdbe1bef6b2f00208a314
Lenny-Prosper <leonardmalcolm00@gmail.com>
2022-12-31 00:58:31 +0000

    Wallet Dropdown JS

--- stake.js
+++ stake.js
@@ -7,8 +7,7 @@ function leftSideMenu() {
     promotions = document.querySelector(".promotions-main"),
     promotionsContainer = document.querySelector(".fl-promotions"),
     sponsorships = document.querySelector(".sponsorships-main"),
-    sponsorshipsContainer = document.querySelector(".fl-sponsorships"),
-    activeDropdown = document.querySelectorAll(".fl-dd-element");
+    sponsorshipsContainer = document.querySelector(".fl-sponsorships");
 
     casino.addEventListener("click", function(){
         casinoContainer.classList.toggle("fl-active");
@@ -26,6 +25,8 @@ function leftSideMenu() {
         sponsorshipsContainer.classList.toggle("fl-active");
     });
 
+
+    const activeDropdown = document.querySelectorAll(".fl-dd-element");
     activeDropdown.forEach(btn => {
         btn.addEventListener("click", function(){
             document.querySelector(".fl-dropdown .fl-dropdown-active").classList.remove("fl-dropdown-active");
@@ -57,25 +58,41 @@ leftSideMenu();
 
 function walletDropdown() {
     const currentWallet = document.querySelector(".ft-current-wallet"),
-    walletOptions = document.querySelector(".wallet-options"),
-    currentCrypto = document.querySelector(".current-v"),
-    selectedCrypto = document.querySelectorAll("#crypto-balance"),
-    currentImage = document.getElementById(".current-image"),
-    selectedImage = document.querySelectorAll("#selected-image"),
-    activeCrypto = document.querySelector(".active-crypto"),
-    wDropdownOptions = document.querySelectorAll(".w-option");
+    walletOptions = document.querySelector(".wallet-options");
 
     currentWallet.addEventListener("click", function(){
         walletOptions.classList.toggle("sw-active");
     });
 
+
+    const currentCrypto = document.querySelector(".current-text .current-value"),
+    wDropdownOptions = document.querySelectorAll(".wallet-dropdown-options .w-option");
+    walletSearch = document.querySelector(".wallet-search input");
+
+    walletSearch.addEventListener("keyup", function() {
+        wsvalue = walletSearch.value.toLowerCase();
+        wDropdownOptions.forEach(crypto => {
+            if(crypto.lastElementChild.textContent.toLowerCase().indexOf(wsvalue) != -1) {
+                crypto.style.display = "flex"
+            }
+            else {
+                crypto.style.display = "none"
+            }
+        })
+    });
+
+
+    currentCrypto.innerHTML = document.querySelector(".wallet-dropdown-options .cv").firstElementChild.innerHTML;
     wDropdownOptions.forEach(element => {
         element.addEventListener("click", () => {
-            currentCrypto.innerHTML = activeCrypto.innerHTML;
-            document.querySelector(".wallet-dropdown-options .active-crypto").classList.remove("active-crypto");
-            element.classList.add("active-crypto");
+            currentCrypto.innerHTML = element.firstElementChild.innerHTML;
+            document.querySelector(".current-text #current-image").src = element.children[1].firstElementChild.src;
+            walletSearch.value = "";
+            walletOptions.classList.toggle("sw-active");
         });
+    
     });
+
 
     const chatIcon = document.querySelector(".chat-icon"),
     chatActive = document.querySelector(".chat");
@@ -88,84 +105,85 @@ function walletDropdown() {
 
 walletDropdown();
 
-const bsPopup = document.querySelectorAll(".menu2-right img"),
-popupItem = document.querySelector(".menu2-right"),
+function oddsType() {
+    const bsPopup = document.querySelectorAll(".menu2-right img"),
+    popupItem = document.querySelector(".menu2-right");
 
-threewayImage = document.querySelector(".three-way img"),
-threeway = document.querySelector(".three-way"),
-standardImage = document.querySelector(".standard img"),
-standard = document.querySelector(".standard"),
+    bsPopup.forEach(img => {
+        img.addEventListener("mouseover", function() {
+            popupItem.classList.add("popup-hover");
+        })
+    });
 
-oddsDisplayType = document.querySelector(".odds-display-type"),
-oddsDisplayToggle = document.querySelectorAll(".menu-all"),
-odTotal = document.querySelector(".od-total"),
-odHandicap = document.querySelector(".od-handicap"),
-odWinner = document.querySelector(".od-winner");
+    bsPopup.forEach(img => {
+        img.addEventListener("mouseout", function() {
+            popupItem.classList.remove("popup-hover");
+        })
+    });
 
 
-bsPopup.forEach(img => {
-    img.addEventListener("mouseover", function() {
-        popupItem.classList.add("popup-hover");
+    const threewayImage = document.querySelector(".three-way img"),
+    threeway = document.querySelector(".three-way"),
+    standardImage = document.querySelector(".standard img"),
+    standard = document.querySelector(".standard");
+
+    threewayImage.addEventListener("click", function() {
+        threeway.style.display = "none";
+        standard.style.display = "block";
+    });
+    
+    standardImage.addEventListener("click", function() {
+        standard.style.display = "none";
+        threeway.style.display = "block";
+    });
+
+
+    const oddsDisplayToggle = document.querySelectorAll(".menu-all"),
+    oddsDisplayType = document.querySelector(".odds-display-type");
+
+    oddsDisplayToggle.forEach(odt => {
+        odt.addEventListener("click", function() {
+           oddsDisplayType.classList.toggle("odds-dd-active");
+        })
     })
-});
 
-bsPopup.forEach(img => {
-    img.addEventListener("mouseout", function() {
-        popupItem.classList.remove("popup-hover");
-    })
-});
 
-threewayImage.addEventListener("click", function() {
-    threeway.style.display = "none";
-    standard.style.display = "block";
-});
+    const odTotal = document.querySelector(".od-total"),
+    odHandicap = document.querySelector(".od-handicap"),
+    odWinner = document.querySelector(".od-winner");
 
-standardImage.addEventListener("click", function() {
-    standard.style.display = "none";
-    threeway.style.display = "block";
-});
-
-oddsDisplayToggle.forEach(odt => {
-    odt.addEventListener("click", function() {
+    odTotal.addEventListener("click", function() {
+        document.querySelector(".bs-menu-winner").style.display = "none";
+        document.querySelector(".bs-menu-handicap").style.display = "none";
+        document.querySelector(".bs-menu-total").style.display = "flex";
+    
+        odHandicap.classList.remove("od-option-active");
+        odWinner.classList.remove("od-option-active");
+        odTotal.classList.add("od-option-active");
         oddsDisplayType.classList.toggle("odds-dd-active");
-    })
-})
+    });
 
+    odHandicap.addEventListener("click", function() {
+        document.querySelector(".bs-menu-winner").style.display = "none";
+        document.querySelector(".bs-menu-handicap").style.display = "flex";
+        document.querySelector(".bs-menu-total").style.display = "none";
+    
+        odHandicap.classList.add("od-option-active");
+        odWinner.classList.remove("od-option-active");
+        odTotal.classList.remove("od-option-active");
+        oddsDisplayType.classList.toggle("odds-dd-active");
+    });
+    
+    odWinner.addEventListener("click", function() {
+        document.querySelector(".bs-menu-winner").style.display = "flex";
+        document.querySelector(".bs-menu-handicap").style.display = "none";
+        document.querySelector(".bs-menu-total").style.display = "none";
+    
+        odHandicap.classList.remove("od-option-active");
+        odWinner.classList.add("od-option-active");
+        odTotal.classList.remove("od-option-active");
+        oddsDisplayType.classList.toggle("odds-dd-active");
+    });
+};
 
-odTotal.addEventListener("click", function() {
-    document.querySelector(".bs-menu-winner").style.display = "none";
-    document.querySelector(".bs-menu-handicap").style.display = "none";
-    document.querySelector(".bs-menu-total").style.display = "flex";
-
-    odHandicap.classList.remove("od-option-active");
-    odWinner.classList.remove("od-option-active");
-    odTotal.classList.add("od-option-active");
-    oddsDisplayType.classList.toggle("odds-dd-active");
-});
-
-odHandicap.addEventListener("click", function() {
-    document.querySelector(".bs-menu-winner").style.display = "none";
-    document.querySelector(".bs-menu-handicap").style.display = "flex";
-    document.querySelector(".bs-menu-total").style.display = "none";
-
-    odHandicap.classList.add("od-option-active");
-    odWinner.classList.remove("od-option-active");
-    odTotal.classList.remove("od-option-active");
-    oddsDisplayType.classList.toggle("odds-dd-active");
-});
-
-odWinner.addEventListener("click", function() {
-    document.querySelector(".bs-menu-winner").style.display = "flex";
-    document.querySelector(".bs-menu-handicap").style.display = "none";
-    document.querySelector(".bs-menu-total").style.display = "none";
-
-    odHandicap.classList.remove("od-option-active");
-    odWinner.classList.add("od-option-active");
-    odTotal.classList.remove("od-option-active");
-    oddsDisplayType.classList.toggle("odds-dd-active");
-});
-
-
-
-
-
+oddsType();
